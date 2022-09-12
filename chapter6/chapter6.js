a3e6258adbb3bff4fea068111690e599a4093086
JFabianMM <103282271+JFabianMM@users.noreply.github.com>
2022-09-12 16:13:27 -0700

    Version 5 of feedback

--- chapter6/chapter6.js
+++ chapter6/chapter6.js
@@ -16,65 +16,82 @@
 // 
 //    Estimated Time: 8 hour.
 
+// The Schema is as follows: The tree is formed with an Array, the first element is 
+// the parent name,  and the following elements will be its children. In the case 
+// of the following example, 'A' is the parent and  'B', 'C' and 'D' its children nodes. 
+// Any node with a string information means have not childrens (are the leaves). 
+// If any of the children is formed by and object, this means is one more branch, 
+// where the first element is the parent and the remain elements its childrens.
+ 
+// Example:  
+// tree=['A', ['B','C','D']]; 
 
-// The way I created a tree is through the arrys that give me the perfect structure 
-// to easily make the relation for parents and childs
 
-// The example shows the three of a root node (with id="container").
-// The result is an array and printed in the console.log.
-// I create the child tree untill the 4 order,  this is:
-// myElement.children.children.children.children.tagName;
-// In the index.html page I create a botton and a square showing the container. 
-// The botton run the function that print the tree (array)  
+// 6) Printing a tree
+// Try to stick to a standard tree node structure. Using an array structure 
+// like the one you are using is not standard. A standard structurer would look something like:
 
-function myFunction() {
-   const myElement = document.getElementById('container');
-   new Array(len).fill(0);
-   let arr1=[];
-   //console.log(myElement.children.length);
-   var len= myElement.children.length;
-   arr1=Array(len).fill(0);
-   //console.log(arr1)
-   if (myElement.children.length>0){     // First Order
-      for (let i = 0; i < myElement.children.length; i++) {
-           arr1[i]=myElement.children[i].tagName;     // Fill the First Order Tag Name
-           // -----------------------------------------------------------------------------
-           if (myElement.children[i].children.length>0){     // Second Order
-               let size2=myElement.children[i].children.length;
-               //console.log(size2)
-               arr1[i]=Array(size2+1).fill(0);
-               arr1[i][0]=myElement.children[i].tagName;       // Fill the First Order Tag Name
-               for (let ii = 0; ii<size2; ii++){
-                  arr1[i][ii+1]=myElement.children[i].children[ii].tagName;    // Fill the Second Order Tag Name
-                  // ----------------------------------------------------------------------------------------------
-                  let element3=myElement.children[i].children[ii];                               
-                  if (element3.children.length>0){               // Third Order
-                      let size3=element3.children.length;
-                      //console.log(size3)
-                      arr1[i][ii+1]=Array(size3+1).fill(0);
-                      arr1[i][ii+1][0]=myElement.children[i].children[ii].tagName;   // Fill the Second Order Tag Name
-                      for (let iii=0; iii<size3; iii++){
-                            arr1[i][ii+1][iii+1]=myElement.children[i].children[ii].children[iii].tagName;  // Fill the Third Order Tag Name
-                            // -----------------------------------------------------------------
-                            let element4=myElement.children[i].children[ii].children[iii];                               
-                            if (element4.children.length>0){               // Third Order
-                                let size4=element4.children.length;
-                                //console.log(size4)
-                                arr1[i][ii+1][iii+1]=Array(size4+1).fill(0);
-                                arr1[i][ii+1][iii+1][0]=myElement.children[i].children[ii].children[iii].tagName;   // Fill the Second Order Tag Name
-                                for (let iiii=0; iiii<size4; iiii++){
-                                      arr1[i][ii+1][iii+1][iiii+1]=myElement.children[i].children[ii].children[iii].children[iiii].tagName;  // Fill the Third Order Tag Name
-                                }             
-                            }
-                      } 
-                  } 
-               }
-           }
-         }
-         console.log(arr1)
-   }
+// const root = {
+// 	  value: ‘A’,
+// 	  children: [
+// 		  { value: ‘a’, children: […]},
+// 		  { value: ‘b’, children: […]},
+// 		  { value: ‘c’: children: […]},
+// 		   …
+//	  ]
+//  };
+
+class Root {
+    constructor(value) {
+      this.value = value;
+      this.children = [];
+    }
 }
 
+function value(arr){
+    let val;
+    let val1;
+    for (let i=0; i<arr.length; i++ ){
+        if (i==0){
+            val=new Root(arr[i]);
+        }else{
+            if (typeof(arr[i])=='string'){
+                val1=new Root(arr[i]);
+                val.children.push(val1);
+            }else{
+                val.children.push(value(arr[i]));
+            }
+        }
+    } 
+    return val;
+}
+
+let tree = ['A',['B',['C','F','G'],'D',['E','H','I']]];         // Example 1
+//let tree=['A',['B','D','E'],'C'];                             // Example 2
+//let tree=['A',['B','C','D']];                                 // Example 3
+
+let root = value(tree);     // First I create the tree structure from an array
+console.log(root);          // I print tree structure
 
 
+// This is the function that traverse a tree structure and get each node’s value.
+function traversetree(root){
+    let result='';
+    for (var [key, value] of Object.entries(root)) {
+        if (key=='value'){
+            result= result + ' '+`${value}`;              
+        }
+        if (key=='children'){
+                if (root.children.length>=1){
+                    for (let i=0;i<root.children.length;i++){
+                        let obj= root.children[i];
+                        result= result + `${traversetree(obj)}`;
+                    }
+                }
+        }   
+    }
+    return result;
+}
+
+console.log(traversetree(root));  // I traverse the tree structure and print each node's value. 
 
